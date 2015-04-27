492e09f0545be4122835f58190820bbefc44f76f
Craig Gidney <craig.gidney@gmail.com>
2015-04-27 03:33:28 -0400

    Entangled states are like Unitary Matrices

---
+++ _posts/2015-04-27-Entangled-States-are-like-Unitary-Matrices.markdown
@@ -0,0 +1,109 @@
+---
+layout: post
+title: "Entangled States are like Unitary Matrices"
+date: 2015-04-25 11:30:00 EST
+categories: quantum
+---
+
+Quantum entanglement is legendarily anti-intuitive. Learning the actual math behind entanglement is infinitely more enlightening than reading or watching pop-sci expositions about it... but you're still left with the question of how to think about entanglement *usefully* when solving problems.
+
+In this post, I describe one trick for thinking about entanglement. It is not an original trick, but I half-stumbled onto it on my own so I'm going to explain it anyways.
+
+**Representing Qubits**
+
+One of the hobby programs I work on from time to time is a quantum circuit simulator. It lets you drag and drop quantum gates onto a circuit, and shows representations of the output state (among other things).
+
+Every quantum system is described by a bunch of complex weights, one for each basis state. You need $2^n$ weights to describe a system with $n$ qubits, because it has $2^n$ basis (i.e. classical) states. This makes visually representing quantum states with more than a few qubits difficult, because there's *so many numbers*.
+
+The first thing I tried, for visually representing the weights of the circuit's output in my simulator, was just a column vector. Each complex number was represented with an oriented circle, and I arranged them vertically in binary order (i.e. from $\ket{0000}$ to $\ket{1111}$). This was problematic, even with just four qubits. Not only was it hard to figure out which weight was where, it used the available space very poorly because it was so thin. So I re-arranged the column into a grid.
+
+Arranging things into a grid turned out better than I expected. Suddenly, operations on half of the qubits had row-wise effects while operations on the other half had column-wise effects. I started using this as a mnemonic tool. When playing with circuits that involved two parties, I'd make distinguishing Alice's effects from Bob's effects easy by giving Alice the row-wise qubits and Bob the column-wise qubits.
+
+Eventually, I noticed that the state grid acted kind of like a matrix. If I had just created an entangled pair of qubits, and applied a gate to one of the qubits, the state would look like the matrix for that gate! And adding another qubit multiplied the "matrix" by the next gate (sortof)! I even got some use out of this: it helped me guess what [symmetry breaking would look like](http://strilanc.com/quantum/2014/12/06/Perfect-Symmetry-Breaking-with-Quantum-Computers.html).
+
+Months later, thanks to learning from [Nielsen and Chuang](http://www.amazon.com/Quantum-Computation-Information-Anniversary-Edition/dp/1107002176) how the [Schmidt Decomposition](http://en.wikipedia.org/wiki/Schmidt_decomposition) related to the [Singular Value Decomposition](http://en.wikipedia.org/wiki/Singular_value_decomposition) and entanglement, I came to understand what was going with my "matrix states".
+
+**State Times Operation**
+
+Consider the state $S\_0 = a \ket{00} + b \ket{10} + c \ket{01} + d \ket{11}$, represented as the matrix $Grid(S\_0) = \begin{bmatrix} a & b \\\\ c & d \end{bmatrix}$. What happens when we apply an operation $U = \begin{bmatrix} e & f \\\\ g & h \end{bmatrix}$ to the first qubit of $S\_0$? (The first qubit is the row-wise one.)
+
+Running the math:
+
+$S\_1 = (U \otimes I) \cdot S\_0$
+
+$= \parens{ U \begin{bmatrix} a \\\\ b \end{bmatrix} } \otimes \ket{0} + \parens{ U \begin{bmatrix} c \\\\ d \end{bmatrix} } \otimes \ket{1}$
+
+$= \begin{bmatrix} ea + bf \\\\ ag + bh \end{bmatrix} \otimes \ket{0} + \begin{bmatrix} ec + df \\\\ cg + dh \end{bmatrix} \otimes \ket{1}$
+
+$= (ea + bf) \ket{00} + (ag + bh) \ket{10} + (ec + df) \ket{01} + (cg + dh) \ket{11}$
+
+Rearranging the final state into a matrix, we find that $Grid(S\_1) = \begin{bmatrix} ea + bf & ag + bh \\\\ ec + df & cg + dh \end{bmatrix}$. This also happens to be equal to $Grid(S\_0) \cdot U^T$. We can compute the results of applying the single-qubit operation while avoiding the tensor product! The same phenomenon occurs when applying $U$ to the second qubit, except there's no transpose and the multiplication happens on the other side: $Grid(S\_2) = U \cdot Grid(S\_0)$.
+
+By representing the state as a matrix, we've simplified how we apply operations. Operating on the first qubit (the row-wise one) is equivalent to post-multiplying the state matrix by the transpose of the operation. Operating on the second qubit (the column-wise one) is equivalent to pre-multiplying the state matrix by the operation. We can even represent some multi-qubit operations (e.g. controlled-nots correspond to flipping the bottom row or right column), but we won't be using any in this post.
+
+Personally, I find it fascinating that the operations simplify like that. It makes it much easier to reason about the states you can reach from some starting state... so let's do that. Let's consider how the single-qubit operations behave when starting from two different states: an unentangled classical "lonely corner" state $\begin{bmatrix} 1 & 0 \\\\ 0 & 0 \end{bmatrix}$ and a completely entangled "shared diagonal" state $\begin{bmatrix} \sqrt{0.5} & 0 \\\\ 0 & \sqrt{0.5} \end{bmatrix}$.
+
+**Case 1: Lonely Corner**
+
+When starting in the matrix state $S\_0 = \begin{bmatrix} 1 & 0 \\\\ 0 & 0 \end{bmatrix}$, what kinds of states can we reach by operating independently on the first and second qubits?
+
+I made a little javascript widget for you to experiment with. See what you can reach by rotating and phasing both qubits. In particular, see if you can reach the entangled shared diagonal state:
+
+<canvas id="drawCanvas1" width="400px" height="410px" style="border:1px dotted #BBB;"/>
+
+Alright, enough toying around, let's analyze things algebraically.
+
+If we apply $U\_1 = \begin{bmatrix} a & b \\\\ c & d \end{bmatrix}$ to the first qubit and $U\_2 = \begin{bmatrix} e & f \\\\ g & h \end{bmatrix}$ to the second qubit (we can always merge multiple operations by multiplying them together, so this is w.l.o.g), we'll get:
+
+$S\_f = U\_2 S\_0 U\_1^T$
+
+$= \begin{bmatrix} e & f \\\\ g & h \end{bmatrix} \begin{bmatrix} 1 & 0 \\\\ 0 & 0 \end{bmatrix} \begin{bmatrix} a & c \\\\ b & d \end{bmatrix}$
+
+$= \begin{bmatrix} e & 0 \\\\ g & 0 \end{bmatrix} \begin{bmatrix} a & b \\\\ c & d \end{bmatrix}$
+
+$= \begin{bmatrix} ae & be \\\\ ag & bg \end{bmatrix}$
+
+$= \begin{bmatrix} e \\\\ g \end{bmatrix} \begin{bmatrix} a & b \end{bmatrix}$
+
+We find that 1) two of the four coefficients from each operation get discarded, and that 2) the resulting state matrix can be described with just two complex ratios (plus a global phase factor that doesn't matter). The horizontal ratio is determined by operations on the first qubit, the vertical ratio is determined by the operations on the second qubit, and that's it.
+
+Examples of the kinds of states we can reach include $\begin{bmatrix} \sqrt{0.5} & \sqrt{0.5} \\\\ 0 & 0 \end{bmatrix}$, $\begin{bmatrix} 0.6 & 0.8 \\\\ 0 & 0 \end{bmatrix}$, $\begin{bmatrix} \sqrt{0.5} & 0 \\\\ -\sqrt{0.5} & 0 \end{bmatrix}$, and $\begin{bmatrix} 0.5 & 0.5i \\\\ 0.5i & -0.5 \end{bmatrix}$.
+
+It's impossible to reach the shared diagonal state from the lonely corner state via independent operations, because the shared diagonal state can't be factored into vertical and horizontal ratios.
+
+**Case 2: Shared Diagonal**
+
+Now let's try starting from the matrix state $S\_0 = \begin{bmatrix} \sqrt{0.5} & 0 \\\\ 0 & \sqrt{0.5} \end{bmatrix}$ and exploring.
+
+If you check closely, you'll notice the system behaves differently in a lot of little ways. For example, there are still states where having both qubits rotating doesn't change the size of the circles much... but they're in a different place than they were in the lonely corner case (finding them is left as an exercise for the reader):
+
+<canvas id="drawCanvas2" width="400px" height="410px" style="border:1px dotted #BBB;"/>
+
+Let's analyze the situation algebraically, again. We apply $U\_1$ to the first qubit and $U\_2$ to the second qubit. We get:
+
+$S\_f = U\_2 S\_0 U\_1^T$
+
+$= U\_2 \begin{bmatrix} \sqrt{0.5} & 0 \\\\ 0 & \sqrt{0.5} \end{bmatrix} U\_1^T$
+
+$= U\_2 \sqrt{0.5} I U\_1^T$
+
+$= \sqrt{0.5} U\_2 U\_1^T$
+
+Well, that's interesting. The starting state is a unitary matrix (times $\sqrt{0.5}$), and all the operations correspond to multiplying the state by a unitary matrix, so the final state is also going to be a unitary matrix (times $\sqrt{0.5}$).
+
+Unlike in the lonely corner case, none of the operations' matrix coefficients are being discarded. Also, the effects of the operations are no longer orthogonal. Instead of one qubit controlling the horizontal, and the other the vertical, they both control it all. Anything Alice does to the first qubit, Bob can undo by applying the appropriate counter-operation to the second qubit. Or Bob can apply the same effect as Alice, effectively squaring the operation. Or Alice, if she knows what Bob will do, can put the system into any (unitary) final state she desires.
+
+Given the above facts, what's something interesting we might be able to do in the shared diagonal case that we couldn't do in the lonely corner case? The first thing that jumps to my mind is that Alice can put *four* numbers into the state now, instead of *two*. That might let us encode more information... wait, that's [superdense coding](http://en.wikipedia.org/wiki/Superdense_coding)! Neat.
+
+**Conclusions and Notes**
+
+In non-entangled systems, the state acts like a matrix made by combining two complex ratios. The ratios are controlled independently and orthogonally by operations on either side.
+
+In entangled systems, the state acts like a unitary matrix. All of the matrix coefficients are controlled by both sides, with overlapping effects. You can move effects between sides, if it's convenient, when designing circuits. Sides can undo or square each others' effects.
+
+Systems that are partially entangled, e.g. $\begin{bmatrix} 0.8 & 0 \\\\ 0 & 0.6 \end{bmatrix}$, can be understood as a linear combination of non-entangled and entangled. The non-entangled and entangled states are kind of like "Singular-Value-Decomposition basis states". Larger systems, with multiple qubits per party (but still just two parties), have more SVD basis states. The SVD is useful as a measure of entanglement because unitary operations preserve it.
+
+This all breaks down if you have more than two parties.
+
+<script src="/assets/2015-04-27-Entangled-States-are-like-Unitary-Matrices.js"></script>
+
---
+++ assets/2015-04-27-Entangled-States-are-like-Unitary-Matrices.js
@@ -0,0 +1,417 @@
+function Complex(r, i) {
+    this.r = r;
+    this.i = i;
+}
+
+var real = function (c) {
+    if (c instanceof Complex) {
+        return c.r;
+    }
+    return float(c);
+};
+
+Complex.from = function (c) {
+    if (c instanceof Complex) {
+        return c;
+    }
+    return new Complex(c, 0);
+};
+
+Complex.prototype.toString = function () {
+    return this.r.toFixed(3) + " + " + this.i.toFixed(3) + "i";
+};
+
+Complex.prototype.plus = function (c) {
+    c = Complex.from(c);
+    return new Complex(this.r + c.r, this.i + c.i);
+};
+
+Complex.prototype.minus = function (c) {
+    c = Complex.from(c);
+    return new Complex(this.r - c.r, this.i - c.i);
+};
+
+Complex.prototype.times = function (c) {
+    c = Complex.from(c);
+    return new Complex(this.r * c.r - this.i * c.i, this.r * c.i + this.i * c.r);
+};
+
+Complex.prototype.phase = function () {
+    return Math.atan2(this.i, this.r);
+};
+
+Complex.prototype.dividedBy = function (c) {
+    c = Complex.from(c);
+    var d = c.norm2();
+    if (d === 0) throw "Division by Zero";
+
+    var n = this.times(c.conjugate());
+    return new Complex(n.r / d, n.i / d);
+};
+
+Complex.prototype.norm2 = function () {
+    return this.r * this.r + this.i * this.i;
+};
+
+Complex.prototype.conjugate = function () {
+    return new Complex(this.r, -this.i);
+};
+
+Complex.prototype.unit = function () {
+    var m = Math.sqrt(this.norm2());
+    if (m < 0.00001) {
+        var theta = Math.atan2(this.i, this.r);
+        return new Complex(Math.cos(theta), -Math.sin(theta));
+    }
+    return this.dividedBy(m);
+};
+
+function Quop(v) {
+    this.m = [Complex.from(v[0]), Complex.from(v[1]), Complex.from(v[2]), Complex.from(v[3])];
+}
+
+Quop.prototype.toString = function () {
+    return "[[" + this.m[0] + ", " + this.m[1] + "], [" + this.m[2] + ", " + this.m[3] + "]]";
+};
+
+Quop.prototype.transpose = function () {
+    return new Quop([this.m[0], this.m[2],
+                     this.m[1], this.m[3]]);
+};
+
+Quop.prototype.adjoint = function () {
+    return new Quop([this.m[0].conjugate(), this.m[2].conjugate(),
+        this.m[1].conjugate(), this.m[3].conjugate()]);
+};
+
+Quop.prototype.scaledBy = function (s) {
+    s = Complex.from(s);
+    return new Quop([this.m[0].times(s), this.m[1].times(s),
+        this.m[2].times(s), this.m[3].times(s)]);
+};
+
+Quop.prototype.plus = function (other) {
+    return new Quop([this.m[0].plus(other.m[0]), this.m[1].plus(other.m[1]),
+        this.m[2].plus(other.m[2]), this.m[3].plus(other.m[3])]);
+};
+
+Quop.prototype.times = function (other) {
+    var a = this.m[0];
+    var b = this.m[1];
+    var c = this.m[2];
+    var d = this.m[3];
+
+    var e = other.m[0];
+    var f = other.m[1];
+    var g = other.m[2];
+    var h = other.m[3];
+
+    return new Quop([
+        a.times(e).plus(b.times(g)), a.times(f).plus(b.times(h)),
+        c.times(e).plus(d.times(g)), c.times(f).plus(d.times(h))]);
+};
+
+Quop.prototype.draw = function (ctx, x, y, d) {
+    var w = d / 2;
+    var r = w / 2;
+
+    // Arrows
+    for (i = 0; i < 4; i++) {
+        var dx = i % 2;
+        var dy = (i - dx) / 2;
+        var cx = x + dx * w + r;
+        var cy = y + dy * w + r;
+        var v = this.m[i];
+
+        ctx.fillStyle = "lightgray";
+        var p = v.norm2();
+        ctx.fillRect(cx - r, cy - r + w*(1-p), w, w*p);
+
+        ctx.beginPath();
+        ctx.arc(cx, cy, Math.sqrt(v.norm2()) * r, 0, 2 * Math.PI);
+        ctx.fillStyle = "yellow";
+        ctx.fill();
+        ctx.strokeStyle = "gray";
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(cx, cy);
+        ctx.lineTo(cx + v.r * r, cy - v.i * r);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+
+        // Text
+        var t = v.toString();
+        ctx.fillStyle = 'black';
+        ctx.fillText(t, cx - ctx.measureText(t).width/2, cy + r - 5);
+        t = ["|00〉", "|01〉", "|10〉", "|11〉"][i];
+        ctx.fillText(t, cx - r + 5, cy - r + 15);
+    }
+
+    // Grid
+    ctx.beginPath();
+    for (var i = 0; i <= 2; i++) {
+        ctx.moveTo(x + w * i, y);
+        ctx.lineTo(x + w * i, y + w * 2);
+
+        ctx.moveTo(x, y + w * i);
+        ctx.lineTo(x + w * 2, y + w * i);
+    }
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+};
+
+var maxBy = function (items, projection) {
+    var hasBest = false;
+    var bestValue = null;
+    var bestWeight = null;
+    for (var i in items) {
+        if (items.hasOwnProperty(i)) {
+            var v = items[i];
+            var w = projection(v);
+            if (!hasBest || w > bestWeight) {
+                hasBest = true;
+                bestValue = v;
+                bestWeight = w;
+            }
+        }
+    }
+    return bestValue;
+};
+
+var squaredSum = function (items) {
+    var total = 0;
+    for (var i in items) {
+        if (items.hasOwnProperty(i)) {
+            var v = items[i];
+            total += v * v;
+        }
+    }
+    return total;
+};
+
+Quop.prototype.ubreakdown = function () {
+    var a = this.m[0];
+    var b = this.m[1];
+    var c = this.m[2];
+    var d = this.m[3];
+
+    var t = a.plus(d).dividedBy(new Complex(0, 2));
+    var x = b.plus(c).dividedBy(new Complex(2, 0));
+    var y = b.minus(c).dividedBy(new Complex(0, -2));
+    var z = a.minus(d).dividedBy(new Complex(2, 0));
+
+    var p = maxBy([t, x, y, z], function (e) {
+        return e.norm2();
+    }).unit();
+    var pt = t.dividedBy(p);
+    var px = x.dividedBy(p);
+    var py = y.dividedBy(p);
+    var pz = z.dividedBy(p);
+
+    return [pt.r, px.r, py.r, pz.r, p];
+};
+
+var sin_scale_ratio = function (theta, factor) {
+    // Near zero, switch to a Taylor series based approximation to avoid floating point error blowup.
+    if (Math.abs(theta) < 0.0001) {
+        var d = theta * theta / 6;
+        return factor * (1 - d * factor * factor) / (1 - d);
+    }
+    return Math.sin(theta * factor) / Math.sin(theta);
+};
+
+var rotation_matrix = function (theta) {
+    var c = Math.cos(theta);
+    var s = Math.sin(theta);
+    return new Quop([c, -s, s, c]);
+};
+
+var phase_cancel_matrix = function (p, q) {
+    p = Complex.from(p);
+    q = Complex.from(q);
+    return new Quop([p.unit().conjugate(), 0, 0, q.unit().conjugate()]);
+};
+
+var singular_value_decomposition_real_2x2 = function (m) {
+    var a = real(m.m[0]);
+    var b = real(m.m[1]);
+    var c = real(m.m[2]);
+    var d = real(m.m[3]);
+
+    var t = a + d;
+    var x = b + c;
+    var y = b - c;
+    var z = a - d;
+
+    var theta_0 = Math.atan2(x, t) / 2.0;
+    var theta_d = Math.atan2(y, z) / 2.0;
+
+    var s_0 = Math.sqrt(t * t + x * x) / 2.0;
+    var s_d = Math.sqrt(z * z + y * y) / 2.0;
+
+    return [
+        rotation_matrix(theta_0 - theta_d),
+        new Quop([s_0 + s_d, 0, 0, s_0 - s_d]),
+        rotation_matrix(theta_0 + theta_d)];
+};
+
+Quop.prototype.svd = function () {
+    var m = this;
+    var p = phase_cancel_matrix(m.m[0], m.m[1]);
+    var m2 = m.times(p);
+
+    // Cancel top-right value by rotation.
+    // m3 = m p r = | ?+?i  0    |
+    //              | ?+?i  ?+?i |
+    var r = rotation_matrix(Math.atan2(m2.m[1].r, m2.m[0].r));
+    var m3 = m2.times(r);
+
+    // Make bottom row non-imaginary and non-negative by column phasing.
+    // m4 = m p r q = | ?+?i  0 |
+    //                | >     > |
+    var q = phase_cancel_matrix(m3.m[2], m3.m[3]);
+    var m4 = m3.times(q);
+
+    // Cancel imaginary part of top left value by row phasing.
+    // m5 = t m p r q = | > 0 |
+    //                  | > > |
+    var t = phase_cancel_matrix(m4.m[0], 1);
+    var m5 = t.times(m4);
+
+    // All values are now real (also the top-right is zero), so delegate to a
+    // singular value decomposition that works for real matrices.
+    // t m p r q = u s v
+    var usv = singular_value_decomposition_real_2x2(m5);
+
+    // m = (t* u) s (v q* r* p*)
+    return [t.adjoint().times(usv[0]),
+        usv[1],
+        usv[2].times(q.adjoint()).times(r.adjoint()).times(p.adjoint())];
+};
+
+Quop.prototype.asStateAfterOperationOn1 = function(op) {
+    return this.times(op.transpose());
+};
+
+Quop.prototype.asStateAfterOperationOn2 = function(op) {
+    return op.times(this);
+};
+
+Quop.prototype.asStateAfterCNotOnto1 = function() {
+    var m = this.m;
+    return new Quop(
+        m[0], m[1],
+        m[3], m[2]);
+};
+
+Quop.prototype.asStateAfterCNotOnto2 = function() {
+    var m = this.m;
+    return new Quop(
+        m[0], m[3],
+        m[2], m[1]);
+};
+
+Quop.rotationMatrix = function(t) {
+    var s = new Complex(Math.sin(t), 0);
+    var c = new Complex(Math.cos(t), 0);
+    return new Quop([c, s.times(-1), s, c]);
+};
+
+Quop.phaseMatrix = function(t) {
+    var s = Math.sin(t);
+    var c = Math.cos(t);
+    return new Quop([new Complex(1, 0), new Complex(0, 0), new Complex(0, 0), new Complex(c, s)]);
+};
+
+var createWidget = function(canvas, s1, s2, title) {
+    var state = new Quop([new Complex(s1, 0), new Complex(0, 0), new Complex(0, 0), new Complex(s2, 0)]);
+    var d = 300;
+    var b = 60;
+    var p = b;
+    var h = 30;
+    var w = 10;
+
+    var buttons = [
+        {x: w + p + d/2 - b/2, y: h, s: "Rotate(1)", on: true},
+        {x: w + p + d*0.75 - b/2, y: h, s: "Phase(1)", on: false},
+        {x: w, y: p + d/2 - b/2 + h, s: "Rotate(2)", on: false},
+        {x: w, y: p + d*0.75 - b/2 + h, s: "Phase(2)", on: false}
+    ];
+    var mousePos = null;
+
+    var t0 = performance.now();
+    var redraw = function () {
+        var t1 = performance.now();
+        var dt = (t1 - t0) / 1000;
+        t0 = t1;
+
+        if (buttons[0].on) {
+            state = state.asStateAfterOperationOn1(Quop.rotationMatrix(dt));
+        }
+        if (buttons[1].on) {
+            state = state.asStateAfterOperationOn1(Quop.phaseMatrix(dt));
+        }
+        if (buttons[2].on) {
+            state = state.asStateAfterOperationOn2(Quop.rotationMatrix(dt));
+        }
+        if (buttons[3].on) {
+            state = state.asStateAfterOperationOn2(Quop.phaseMatrix(dt));
+        }
+
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.font = "20px Helvetica";
+        ctx.fillStyle = 'gray';
+        ctx.fillText("Click to toggle ----->", w, b/2 + 8 + h);
+        ctx.fillStyle = 'black';
+        ctx.fillText(title, w + b + d/2 - ctx.measureText(title).width/2, h/2+6)
+        ctx.font = "12px Helvetica";
+        for (var i = 0; i < buttons.length; i++) {
+            var s = buttons[i].s;
+            var x = buttons[i].x;
+            var y = buttons[i].y;
+            ctx.strokeStyle = 'black';
+            ctx.fillStyle = buttons[i].on ? '#0F0' : '#FFF';
+            if (mousePos !== null && mousePos[0] > x && mousePos[1] > y && mousePos[0] < x + b && mousePos[1] < y + b) {
+                ctx.fillStyle = buttons[i].on ? '#2D2' : '#CCC';
+            }
+            ctx.fillRect(x, y, b, b);
+            ctx.strokeRect(x, y, b, b);
+            ctx.fillStyle = 'black';
+            ctx.fillText(s, x + b / 2 - ctx.measureText(s).width/2, y + b / 2 + 6);
+            ctx.fillText(s, x + b / 2 - ctx.measureText(s).width/2, y + b / 2 + 6);
+        }
+
+        state.draw(ctx, p + w, p + h, d);
+    };
+
+    $(canvas).mousemove(function(e) {
+        var x = e.pageX - $(canvas).offset().left;
+        var y = e.pageY - $(canvas).offset().top;
+        mousePos = [x, y];
+        redraw();
+    });
+    $(canvas).mouseleave(function() {
+        mousePos = null;
+        redraw();
+    });
+    $(canvas).click(function(e) {
+        mousePos = [e.pageX - $(canvas).offset().left, e.pageY - $(canvas).offset().top];
+        for (var i = 0; i < buttons.length; i++) {
+            var x = buttons[i].x;
+            var y = buttons[i].y;
+            if (mousePos[0] > x && mousePos[1] > y && mousePos[0] < x + b && mousePos[1] < y + b) {
+                buttons[i].on ^= true;
+            }
+        }
+        redraw();
+    });
+
+    setInterval(redraw, 50);
+};
+
+createWidget(document.getElementById("drawCanvas1"), 1, 0, "Operate on Independent Qubits");
+createWidget(document.getElementById("drawCanvas2"), Math.sqrt(0.5), Math.sqrt(0.5), "Operate on Entangled Qubits");
+
