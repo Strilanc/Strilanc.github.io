646ff3e500c31a0722cd8a6d3b8b64d5c856f7c2
Craig Gidney <craig.gidney@gmail.com>
2015-04-28 11:02:19 -0400

    Debugging new posts not appearing. 8: binary search script

--- assets/2015-04-27-Entangled-States-are-like-Unitary-Matrices.js
+++ assets/2015-04-27-Entangled-States-are-like-Unitary-Matrices.js
@@ -161,3 +161,133 @@ Quop.prototype.draw = function (ctx, x, y, d) {
     ctx.stroke();
 };
 
+var maxBy = function (items, projection) {
+    var hasBest = false;
+    var bestValue = null;
+    var bestWeight = null;
+    for (var i in items) {
+        if (items.hasOwnProperty(i)) {
+            var v = items[i];
+            var w = projection(v);
+            if (!hasBest || w > bestWeight) {
+                hasBest = true;
+                bestValue = v;
+                bestWeight = w;
+            }
+        }
+    }
+    return bestValue;
+};
+
+var squaredSum = function (items) {
+    var total = 0;
+    for (var i in items) {
+        if (items.hasOwnProperty(i)) {
+            var v = items[i];
+            total += v * v;
+        }
+    }
+    return total;
+};
+
+Quop.prototype.ubreakdown = function () {
+    var a = this.m[0];
+    var b = this.m[1];
+    var c = this.m[2];
+    var d = this.m[3];
+
+    var t = a.plus(d).dividedBy(new Complex(0, 2));
+    var x = b.plus(c).dividedBy(new Complex(2, 0));
+    var y = b.minus(c).dividedBy(new Complex(0, -2));
+    var z = a.minus(d).dividedBy(new Complex(2, 0));
+
+    var p = maxBy([t, x, y, z], function (e) {
+        return e.norm2();
+    }).unit();
+    var pt = t.dividedBy(p);
+    var px = x.dividedBy(p);
+    var py = y.dividedBy(p);
+    var pz = z.dividedBy(p);
+
+    return [pt.r, px.r, py.r, pz.r, p];
+};
+
+var sin_scale_ratio = function (theta, factor) {
+    // Near zero, switch to a Taylor series based approximation to avoid floating point error blowup.
+    if (Math.abs(theta) < 0.0001) {
+        var d = theta * theta / 6;
+        return factor * (1 - d * factor * factor) / (1 - d);
+    }
+    return Math.sin(theta * factor) / Math.sin(theta);
+};
+
+var rotation_matrix = function (theta) {
+    var c = Math.cos(theta);
+    var s = Math.sin(theta);
+    return new Quop([c, -s, s, c]);
+};
+
+var phase_cancel_matrix = function (p, q) {
+    p = Complex.from(p);
+    q = Complex.from(q);
+    return new Quop([p.unit().conjugate(), 0, 0, q.unit().conjugate()]);
+};
+
+var singular_value_decomposition_real_2x2 = function (m) {
+    var a = real(m.m[0]);
+    var b = real(m.m[1]);
+    var c = real(m.m[2]);
+    var d = real(m.m[3]);
+
+    var t = a + d;
+    var x = b + c;
+    var y = b - c;
+    var z = a - d;
+
+    var theta_0 = Math.atan2(x, t) / 2.0;
+    var theta_d = Math.atan2(y, z) / 2.0;
+
+    var s_0 = Math.sqrt(t * t + x * x) / 2.0;
+    var s_d = Math.sqrt(z * z + y * y) / 2.0;
+
+    return [
+        rotation_matrix(theta_0 - theta_d),
+        new Quop([s_0 + s_d, 0, 0, s_0 - s_d]),
+        rotation_matrix(theta_0 + theta_d)];
+};
+
+Quop.prototype.svd = function () {
+    var m = this;
+    var p = phase_cancel_matrix(m.m[0], m.m[1]);
+    var m2 = m.times(p);
+
+    // Cancel top-right value by rotation.
+    // m3 = m p r = | ?+?i  0    |
+    //              | ?+?i  ?+?i |
+    var r = rotation_matrix(Math.atan2(m2.m[1].r, m2.m[0].r));
+    var m3 = m2.times(r);
+
+    // Make bottom row non-imaginary and non-negative by column phasing.
+    // m4 = m p r q = | ?+?i  0 |
+    //                | >     > |
+    var q = phase_cancel_matrix(m3.m[2], m3.m[3]);
+    var m4 = m3.times(q);
+
+    // Cancel imaginary part of top left value by row phasing.
+    // m5 = t m p r q = | > 0 |
+    //                  | > > |
+    var t = phase_cancel_matrix(m4.m[0], 1);
+    var m5 = t.times(m4);
+
+    // All values are now real (also the top-right is zero), so delegate to a
+    // singular value decomposition that works for real matrices.
+    // t m p r q = u s v
+    var usv = singular_value_decomposition_real_2x2(m5);
+
+    // m = (t* u) s (v q* r* p*)
+    return [t.adjoint().times(usv[0]),
+        usv[1],
+        usv[2].times(q.adjoint()).times(r.adjoint()).times(p.adjoint())];
+};
+
+
